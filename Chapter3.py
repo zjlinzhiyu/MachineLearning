16ee65947cea9d9456a566ca530e303aff32659b
Blinglin_co_deepin <342753915@qq.com>
2018-10-22 10:33:43 +0800

    add files

---
+++ Chapter3.py
@@ -0,0 +1,57 @@
+from sklearn import datasets
+import numpy as np
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+from sklearn.linear_model import Perceptron
+ppn = Perceptron(max_iter=40, eta0=0.1, random_state=0)
+ppn.fit(X_train_std, y_train)
+y_pred = ppn.predict(X_test_std)
+print('Misclassified samples: %d' % (y_test != y_pred).sum())
+
+
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
+
+
+
+
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+plot_decision_regions(X=X_combined_std,
+                      y=y_combined,
+                      classifier=ppn,
+                      test_idx=range(105, 150))
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [stamdarized]')
+plt.legend(loc='upper left')
+plt.show()
+
+import matplotlib.pyplot as plt
+import numpy as np
+def sigmoid(z):
+    return 1.0 / (1.0 + np.exp(-z))
+z = np.arange(-7, 7, 0.1)
+phi_z = sigmoid(z)
+plt.plot(z, phi_z)
+plt.axvline(0.0, color='k')
+plt.axhspan(0.1, 1.0, facecolor='0.0', alpha=0.2, ls='dotted')
+plt.axhline(y=0.5, ls='dotted', color='k')
+plt.yticks([0.0, 0.5, 1.0])
+plt.ylim(-0.1, 1.1)
+plt.xlabel('z')
+plt.ylabel('$\phi (z)$')
+plt.show()
+
---
+++ run.py
@@ -0,0 +1,221 @@
+#-*-coding:utf-8 -*-
+
+from sklearn import datasets
+import numpy as np
+
+iris = datasets.load_iris()
+X = iris.data[:, [2, 3]]
+y = iris.target
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+sc = StandardScaler()
+sc.fit(X_train)
+X_train_std = sc.transform(X_train)
+X_test_std = sc.transform(X_test)
+
+from sklearn.linear_model import LogisticRegression
+from packages.plot_decision_regions import plot_decision_regions
+pdr = plot_decision_regions
+lr = LogisticRegression(C=1000.0, random_state=0)
+lr.fit(X_train_std, y_train)
+X_combined_std = np.vstack((X_train_std, X_test_std))
+y_combined = np.hstack((y_train, y_test))
+pdr(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
+
+import matplotlib.pyplot as plt
+plt.xlabel('petal length [standardized]')
+plt.ylabel('petal width [standardized')
+plt.legend(loc='upper left')
+plt.show()
+
+lr.predict_proba(X_test_std[0, :])
+
+#____________________________________________________________
+
+## Chapter4
+#NaN 举例
+import pandas as pd
+from io import StringIO
+csv_data = '''A,B,C,D
+1.0,2.0.,3.0,4.0
+5.0,6.0,,8.0
+0.0,11.0,12.0,'''
+df = pd.read_csv(StringIO(csv_data))
+
+df.isnull().sum()
+
+#Imputing missing values
+from sklearn.preprocessing import Imputer
+imr = Imputer(missing_values='NaN', strategy='mean', axis=0)
+imr = imr.fit(df)
+imputed_data = imr.transform(df.values)
+imputed_data
+
+
+#Handling categorical data
+import pandas as pd
+df = pd.DataFrame([
+    ['green', 'M', 10.1, 'class1'],
+    ['red', 'L', 13.5, 'class2'],
+    ['blue', 'XL', 15.3, 'class1']
+])
+df.columns = ['color', 'size', 'price', 'classlabel']
+
+#mapping ordial features
+size_mapping = {
+    'XL': 3,
+    'L': 2,
+    'M': 1
+}
+df['size'] = df['size'].map(size_mapping)
+
+inv_size_mapping = {v: k for k, v in size_mapping.items()}
+
+#encoding class label
+import numpy as np
+class_mapping = {label: idx for idx, label in enumerate(np.unique(df['classlabel']))}
+df['classlabel'] = df['classlabel'].map(class_mapping)
+
+inv_class_mapping = {v: k for k, v in class_mapping.items()}
+df['classlabel'] = df['classlabel'].map(inv_class_mapping)
+
+from sklearn.preprocessing import LabelEncoder
+class_le = LabelEncoder()
+y = class_le.fit_transform(df['classlabel'].values)
+
+class_le.inverse_transform(y)
+
+#Performing one-hot encoding on nominal features
+X = df[['color', 'size', 'price']].values
+color_le = LabelEncoder()
+X[:, 0] = color_le.fit_transform(X[:, 0])
+
+from sklearn.preprocessing import OneHotEncoder
+ohe = OneHotEncoder(categorical_features=[0])
+ohe.fit_transform(X).toarray()
+
+#get_dummies方法会改变字符串列，不改变其他列
+pd.get_dummies(df[['price', 'color', 'size']])
+
+#Partioning a dataset in training and test sets(Wine data)
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header = None)
+df_wine.columns = ['Class label', 'Alcohol',
+                   'Malic acid', 'Ash',
+                   'Alcalinity of ash', 'Magnesium',
+                   'Total phenols', 'Flavanoids',
+                   'Nonflavanoid phenols',
+                   'Proanthocyanins',
+                   'Color intensity', 'Hue',
+                   'OD280/OD315 of diluted wines',
+                   'Proline']
+print('Class labels', np.unique(df_wine['Class label']))
+df_wine.head()
+
+from sklearn.model_selection import train_test_split
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+
+#bring feastures onto the same scale
+# normalization
+from sklearn.preprocessing import MinMaxScaler
+mms = MinMaxScaler()
+X_train_norm = mms.fit_transform(X_train)
+X_test_norm = mms.transform(X_test)
+
+#standardization
+from sklearn.preprocessing import StandardScaler
+stdsc = StandardScaler()
+X_train_std = stdsc.fit_transform(X_train)
+X_test_std = stdsc.transform(X_test)
+
+
+# sparse solutions with L1 regularization
+from sklearn.linear_model import LogisticRegression
+LogisticRegression(penalty='l1')
+
+lr = LogisticRegression(penalty='l1', C=0.1)
+lr.fit(X_train_std, y_train)
+print('Training accuracy:', lr.score(X_train_std, y_train))
+
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = plt.subplot(111)
+colors = ['blue', 'green', 'red', 'cyan', 'magenta', 'yellow',
+          'black', 'pink', 'lightgreen', 'lightblue', 'gray',
+          'indigo', 'orange']
+weights, params =[], []
+for c in np.arange(-4, 6):
+    lr = LogisticRegression(penalty='l1',
+                            C=10**int(c),
+                            random_state=0)
+    lr.fit(X_train_std, y_train)
+    weights.append(lr.coef_[1])
+    params.append(10**int(c))
+
+weights = np.array(weights)
+for column, color in zip(range(weights.shape[1]), colors):
+    plt.plot(params, weights[:, column],
+             label=df_wine.columns[column+1],
+             color=color)
+
+plt.axhline(0, color='black', linestyle='--', linewidth=3)
+plt.xlim([10**(-5), 10**5])
+plt.ylabel('weight coefficient')
+plt.xlabel('C')
+plt.xscale('log')
+plt.legend(loc='upper left')
+ax.legend(loc='upper center',
+          bbox_to_anchor=(1.38, 1.03),
+          ncol=1, fancybox=True)
+plt.show()
+#显示不全时可以使用下列命令
+plt.tight_layout()
+
+# SBS演示
+from sklearn.neighbors import KNeighborsClassifier
+import matplotlib.pyplot as plt
+knn = KNeighborsClassifier(n_neighbors=2)
+sbs = SBS(knn, k_features=1)
+sbs.fit(X_train_std, y_train)
+
+k_feat = [len(k) for k in sbs.subsets_]
+plt.plot(k_feat, sbs.scores_, marker='o')
+plt.ylim([0.7, 1.1])
+plt.ylabel('Accurady')
+plt.xlabel('Number of features')
+plt.grid()
+plt.show()
+
+
+#Assessing feature importance with random forests
+from sklearn.ensemble import RandomForestClassifier
+feat_labels = df_wine.columns[1:]
+forest = RandomForestClassifier(n_estimators=10000,
+                                 random_state=0,
+                                 n_jobs=-1)
+forest.fit(X_train, y_train)
+importances = forest.feature_importances_
+indices = np.argsort(importances)[::-1]
+for f in range(X_train.shape[1]):
+    print("%2d)  %-*s %f" % (f+1, 30,
+                             feat_labels[f],
+                             importances[indices[f]]))
+
+plt.title('Feature Importance')
+plt.bar(range(X_train.shape[1]),
+        importances[indices],
+        color='lightblue',
+        align='center')
+plt.xticks(range(X_train.shape[1]),
+           feat_labels, rotation=90)
+plt.xlim([-1, X_train.shape[1]])
+plt.tight_layout()
+plt.show()
+
+#——————————————————————————————————————————————————————————————
+
+#Chapter 5 Compressing Data via Dimensionality Reduction
+## Unsuoervised dimensionality reduction via principal component analysis
