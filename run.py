14bf60fa525619faffde10f596efe18eb7b8f381
Blinglin_co_deepin <342753915@qq.com>
2018-10-23 09:41:31 +0800

    Eigenvalues

--- run.py
+++ run.py
@@ -219,3 +219,20 @@ plt.show()
 
 #Chapter 5 Compressing Data via Dimensionality Reduction
 ## Unsuoervised dimensionality reduction via principal component analysis
+import pandas as pd
+df_wine = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/wine/wine.data', header=None)
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
+X_train, X_test, y_train, y_test = \
+        train_test_split(X, y, test_size=0.3,
+                         random_state=0)
+sc = StandardScaler()
+X_train_std = sc.fit_transform(X_train)
+X_test_std = sc.fit_transform(X_test)
+
+import numpy as np
+cov_mat = np.cov(X_train_std.T)
+eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
+print('\nEigenvalues \n%s' % eigen_vals)
